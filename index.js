5f552d47567dfb60b9428d70ce2fd8c3a079d06e
shahateqx <shahat0209@gmail.com>
2026-07-20 17:50:39 +0600

    Stage 4: full CRUD

--- index.js
+++ index.js
@@ -74,6 +74,50 @@ app.post("/tasks", (req, res) => {
     res.status(201).json(newTask);
 });
 
+app.put("/tasks/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const { title, done } = req.body;
+
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    if (
+        title === undefined ||
+        title.trim() === "" ||
+        typeof done !== "boolean"
+    ) {
+        return res.status(400).json({
+            error: "Title and done are required"
+        });
+    }
+
+    task.title = title;
+    task.done = done;
+
+    res.json(task);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const taskIndex = tasks.findIndex(task => task.id === id);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    tasks.splice(taskIndex, 1);
+
+    res.sendStatus(204);
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
